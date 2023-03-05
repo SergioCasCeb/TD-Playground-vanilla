4c7db296552e03ba8f37bf376668ca0c0541f4ae
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-03-05 17:57:07 +0100

    new logo, better resizing functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+const textIcon = document.querySelectorAll(".text-icon");
 /**** Ace editor ****/
 const editorElement = document.querySelector("#editor");
 const editor = ace.edit("editor");
 editor.setOptions({
-    mode: "ace/mode/javascript",
-    selectionStyle: "text",
-    theme: "ace/theme/chrome",
-    showPrintMargin: false,
-    displayIndentGuides: true,
-    scrollPastEnd: 0.5
+  mode: "ace/mode/javascript",
+  selectionStyle: "text",
+  theme: "ace/theme/chrome",
+  showPrintMargin: false,
+  displayIndentGuides: true,
+  scrollPastEnd: 0.5
 });
 
 /***** Resizing functionality *****/
@@ -16,85 +17,112 @@ const resizerX = document.querySelector(".vertical-divider");
 
 /*** Vertical sizing ***/
 resizerX.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    document.addEventListener("mousemove", onmousemove);
-    document.addEventListener("mouseup", onmouseup);
+  e.preventDefault();
+  document.addEventListener("mousemove", onmousemove);
+  document.addEventListener("mouseup", onmouseup);
 })
 
 function onmousemove(e) {
-    e.preventDefault();
-    let clientX = e.clientX;
-    const deltaX = clientX - (resizerX._clientX || clientX);
-    resizerX._clientX = clientX;
-    const l = resizerX.previousElementSibling;
-    const r = resizerX.nextElementSibling;
+  e.preventDefault();
+  let clientX = e.clientX;
+  const deltaX = clientX - (resizerX.clientX || clientX);
+  const l = resizerX.previousElementSibling;
+  const r = resizerX.nextElementSibling;
+
+  if(clientX > screen.width){
+    resizerX.clientX = screen.width;
+  }
+  else if(clientX < 0){
+    resizerX.clientX = 0;
+  }
+  else{
+    resizerX.clientX = clientX;
 
     // LEFT
     if (deltaX < 0) {
       const w = Math.round(parseInt(getComputedStyle(l).width) + deltaX);
-      console.log("wL: ", w);
-      l.style.flex = `0 ${w < 35 ? 35 : w}px`;
+      l.style.flex = `0 ${w < 45 ? 30 : w}px`;
       r.style.flex = "1 0";
+      if( w < 65 ){ 
+        textIcon.forEach (text => {
+          text.classList.add("hiddenH");
+        })
+      }
     }
 
     // RIGHT
-    console.log(deltaX);
     if (deltaX > 0) {
-      const w = Math.round(parseInt(getComputedStyle(r).width) - deltaX);
-      console.log("wR: ", w);
-      if( w <= 35 ){
-        clientX = 1851;
-        console.log(clientX); 
-        delete e._clientX;
+      const w = Math.round(parseInt(getComputedStyle(l).width) + deltaX);
+      console.log(w);
+      l.style.flex = `0 ${w > 65 ? 80 : w}px`;
+      r.style.flex = "1 0";
+      if( w > 65 ){ 
+        textIcon.forEach (text => {
+          text.classList.remove("hiddenH");
+        })
       }
-      r.style.flex = `0 ${w < 35 ? 36 : w}px`;
-      l.style.flex = "1 0";
     }  
+  } 
 }
 
 function onmouseup(e) {
-    e.preventDefault();
-    document.removeEventListener("mousemove", onmousemove);
-    document.removeEventListener("mouseup", onmouseup);
-    delete e._clientX;
+  e.preventDefault();
+  document.removeEventListener("mousemove", onmousemove);
+  document.removeEventListener("mouseup", onmouseup);
+  delete e.clientX;
 }
 
 
 /*** Vertical sizing ***/
 resizerY.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    document.addEventListener("mousemove", onmousemoveY);
-    document.addEventListener("mouseup", onmouseupY);
-    console.log("mouse donw");
-  })
-  
-  function onmousemoveY(e) {
-    e.preventDefault();
-    const clientY = e.clientY;
-    const deltaY = clientY - (resizerY._clientY || clientY);
-    resizerY._clientY = clientY;
-    const t = resizerY.previousElementSibling;
-    const b = resizerY.nextElementSibling;
+  e.preventDefault();
+  document.addEventListener("mousemove", onmousemoveY);
+  document.addEventListener("mouseup", onmouseupY);
+})
 
-    console.log(deltaY);
-    // UP
-    if (deltaY < 0) {
-      const h = Math.round(parseInt(getComputedStyle(t).height) + deltaY);
-      t.style.flex = `0 ${h < 10 ? 10 : h}px`;
-      b.style.flex = "1 0";
-    }
+function onmousemoveY(e) {
+  e.preventDefault();
+  const clientY = e.clientY;
+  const deltaY = clientY - (resizerY.clientY || clientY);
+  const t = resizerY.previousElementSibling;
+  const b = resizerY.nextElementSibling;
+
+  if(clientY > screen.height){
+    resizerY.clientY = screen.height;
+  }
+  else if(clientY < 0){
+    resizerY.clientY = 0;
+  }
+  else{
+    resizerY.clientY = clientY;
     // DOWN
     if (deltaY > 0) {
       const h = Math.round(parseInt(getComputedStyle(b).height) - deltaY);
-      b.style.flex = `0 ${h < 10 ? 10 : h}px`;
+      b.style.flex = `0 ${h < 55 ? 50 : h}px`;
       t.style.flex = "1 0";
+      if( Math.round(parseInt(getComputedStyle(t).height) + deltaY) > 290 ){ 
+        textIcon.forEach (text => {
+          text.classList.remove("hiddenV");
+        })
+      }
     }
+    // UP
+    if (deltaY < 0) {
+      const h = Math.round(parseInt(getComputedStyle(t).height) + deltaY);
+      t.style.flex = `0 ${h < 155 ? 145 : h}px`;
+      b.style.flex = "1 0";
+      if( h < 290 ){ 
+        textIcon.forEach (text => {
+          text.classList.add("hiddenV");
+        })
+      }
+    }
+    
   }
-  function onmouseupY(e) {
-    e.preventDefault(); 
-    document.removeEventListener("mousemove", onmousemoveY);
-    document.removeEventListener("mouseup", onmouseupY);
-    delete e._clientY;
-  }
-
-
+}
+function onmouseupY(e) {
+  e.preventDefault(); 
+  document.removeEventListener("mousemove", onmousemoveY);
+  document.removeEventListener("mouseup", onmouseupY);
+  delete e._clientY;
+}
