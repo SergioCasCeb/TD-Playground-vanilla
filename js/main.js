e115a63e33e49bb050ac763d03391e70821e2eb1
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-04-06 14:08:18 +0200

    adding/deletting new ide, settings functionality

--- js/main.js
+++ js/main.js
@@ -9,58 +9,58 @@ const themeData = {
   "inherit": true,
   "rules": [
     { 'token': '', 'foreground': 'ffffff', 'background': '549087' },
-		{ 'token': 'invalid', 'foreground': 'f44747' },
-		{ 'token': 'emphasis', 'fontStyle': 'italic' },
-		{ 'token': 'strong', 'fontStyle': 'bold' },
+    { 'token': 'invalid', 'foreground': 'f44747' },
+    { 'token': 'emphasis', 'fontStyle': 'italic' },
+    { 'token': 'strong', 'fontStyle': 'bold' },
 
     { 'token': 'variable', 'foreground': 'ffbb00' },
-		{ 'token': 'variable.predefined', 'foreground': 'ffbb00' },
-		{ 'token': 'variable.parameter', 'foreground': 'ffbb00' },
-		{ 'token': 'constant', 'foreground': 'ffbb00' },
-		{ 'token': 'comment', 'foreground': '7b2d8a' },
-		{ 'token': 'number', 'foreground': 'fffffe' },
-		{ 'token': 'number.hex', 'foreground': 'fffffe' },
-		{ 'token': 'regexp', 'foreground': 'be3989' },
-		{ 'token': 'annotation', 'foreground': 'cc6666' },
-		{ 'token': 'type', 'foreground': '00fcce' },
-    
+    { 'token': 'variable.predefined', 'foreground': 'ffbb00' },
+    { 'token': 'variable.parameter', 'foreground': 'ffbb00' },
+    { 'token': 'constant', 'foreground': 'ffbb00' },
+    { 'token': 'comment', 'foreground': '7b2d8a' },
+    { 'token': 'number', 'foreground': 'fffffe' },
+    { 'token': 'number.hex', 'foreground': 'fffffe' },
+    { 'token': 'regexp', 'foreground': 'be3989' },
+    { 'token': 'annotation', 'foreground': 'cc6666' },
+    { 'token': 'type', 'foreground': '00fcce' },
+
     { token: 'delimiter', foreground: 'ffffff' },
-		{ token: 'delimiter.html', foreground: '808080' },
-		{ token: 'delimiter.xml', foreground: '808080' },
-    
+    { token: 'delimiter.html', foreground: '808080' },
+    { token: 'delimiter.xml', foreground: '808080' },
+
     { token: 'tag', foreground: '7b2d8a' },
-		{ token: 'tag.id.pug', foreground: '7b2d8a' },
-		{ token: 'tag.class.pug', foreground: '7b2d8a' },
-		{ token: 'meta.scss', foreground: 'A79873' },
-		{ token: 'meta.tag', foreground: 'd1744f' },
-		{ token: 'metatag', foreground: 'DD6A6F' },
-		{ token: 'metatag.content.html', foreground: 'ffbb00' },
-		{ token: 'metatag.html', foreground: 'ffbb00' },
-		{ token: 'metatag.xml', foreground: 'ffbb00' },
-		{ token: 'metatag.php', fontStyle: 'bold' },
-    
+    { token: 'tag.id.pug', foreground: '7b2d8a' },
+    { token: 'tag.class.pug', foreground: '7b2d8a' },
+    { token: 'meta.scss', foreground: 'A79873' },
+    { token: 'meta.tag', foreground: 'd1744f' },
+    { token: 'metatag', foreground: 'DD6A6F' },
+    { token: 'metatag.content.html', foreground: 'ffbb00' },
+    { token: 'metatag.html', foreground: 'ffbb00' },
+    { token: 'metatag.xml', foreground: 'ffbb00' },
+    { token: 'metatag.php', fontStyle: 'bold' },
+
     { token: 'key', foreground: 'ffbb00' },
-		{ token: 'string.key.json', foreground: 'ffbb00' },
-		{ token: 'string.value.json', foreground: 'd06c44' },
-    
+    { token: 'string.key.json', foreground: 'ffbb00' },
+    { token: 'string.value.json', foreground: 'd06c44' },
+
     { token: 'attribute.name', foreground: 'ffbb00' },
-		{ token: 'attribute.value', foreground: 'd06c44' },
-		{ token: 'attribute.value.number.css', foreground: '6ca74c' },
-		{ token: 'attribute.value.unit.css', foreground: '6ca74c' },
-		{ token: 'attribute.value.hex.css', foreground: 'D4D4D4' },
-    
+    { token: 'attribute.value', foreground: 'd06c44' },
+    { token: 'attribute.value.number.css', foreground: '6ca74c' },
+    { token: 'attribute.value.unit.css', foreground: '6ca74c' },
+    { token: 'attribute.value.hex.css', foreground: 'D4D4D4' },
+
     { token: 'string', foreground: 'd06c44' },
-		{ token: 'string.sql', foreground: 'FF0000' },
-    
+    { token: 'string.sql', foreground: 'FF0000' },
+
     { token: 'keyword', foreground: 'ffbb00' },
-		{ token: 'keyword.flow', foreground: 'b33ea9' },
-		{ token: 'keyword.json', foreground: 'd06c44' },
-		{ token: 'keyword.flow.scss', foreground: 'ffbb00' },
-    
+    { token: 'keyword.flow', foreground: 'b33ea9' },
+    { token: 'keyword.json', foreground: 'd06c44' },
+    { token: 'keyword.flow.scss', foreground: 'ffbb00' },
+
     { token: 'operator.scss', foreground: '909090' },
-		{ token: 'operator.sql', foreground: 'ffbb00' },
-		{ token: 'operator.swift', foreground: '909090' },
-		{ token: 'predefined.sql', foreground: 'FF00FF' },
+    { token: 'operator.sql', foreground: 'ffbb00' },
+    { token: 'operator.swift', foreground: '909090' },
+    { token: 'predefined.sql', foreground: 'FF00FF' },
   ],
   "colors": {
     "editor.foreground": "#FFFFFF",
@@ -69,7 +69,7 @@ const themeData = {
     "editor.lineHighlightBackground": "#067362",
     "editorCursor.foreground": "#FFFFFF"
   }
-  
+
 }
 /** Monaco editor **/
 require.config({ paths: { vs: '../monaco-editor/min/vs' } });
@@ -79,16 +79,16 @@ require(['vs/editor/editor.main'], function () {
     value: ['{',
       '  "id": "urn:simple",',
       '  "@context": "https://www.w3.org/2022/wot/td/v1.1",',
-      '  "title": "MyLampThing",',
-      '  "description": "Valid TD copied from the specs first example",',
+      '  "title": "My Thing 1",',
+      '  "description": "Empty thing description",',
       '  "securityDefinitions": {',
-          '    "basic_sc": {',
-              '      "scheme": "basic",',
-              '      "in": "header"',
-          '    }',
+      '    "basic_sc": {',
+      '      "scheme": "basic",',
+      '      "in": "header"',
+      '    }',
       '  },',
       '  "security": [',
-          '    "basic_sc"',
+      '    "basic_sc"',
       '  ],',
       '  "properties": {',
       '  },',
@@ -96,13 +96,25 @@ require(['vs/editor/editor.main'], function () {
       '  },',
       '  "events": {',
       '  }',
-  '}'].join('\n'),
+      '}'].join('\n'),
     language: 'json',
     automaticLayout: true
   });
 
   monaco.editor.defineTheme('monochrome', themeData);
   document.onload = setTheme()
+  document.onload = setFontSize(editor)
+  fontSizeSlider.addEventListener("input", () => {
+    fontSizeTxt.innerText = fontSizeSlider.value
+    storeFontSize(fontSizeSlider.value)
+    setFontSize(editor)
+  })
+  editorForm.addEventListener("reset", (e) => {
+    e.preventDefault()
+    fontSizeSlider.value = 14
+    storeFontSize(fontSizeSlider.value)
+    setFontSize(editor)
+  })
 });
 
 /***** Resizing functionality *****/
@@ -123,13 +135,13 @@ function onmousemove(e) {
   const l = resizerX.previousElementSibling;
   const r = resizerX.nextElementSibling;
 
-  if(clientX > screen.width){
+  if (clientX > screen.width) {
     resizerX.clientX = screen.width;
   }
-  else if(clientX < 0){
+  else if (clientX < 0) {
     resizerX.clientX = 0;
   }
-  else{
+  else {
     resizerX.clientX = clientX;
 
     // LEFT
@@ -137,8 +149,8 @@ function onmousemove(e) {
       const w = Math.round(parseInt(getComputedStyle(l).width) + deltaX);
       l.style.flex = `0 ${w < 45 ? 30 : w}px`;
       r.style.flex = "1 0";
-      if( w < 65 ){ 
-        textIcon.forEach (text => {
+      if (w < 65) {
+        textIcon.forEach(text => {
           text.classList.add("hiddenH");
         })
       }
@@ -147,16 +159,15 @@ function onmousemove(e) {
     // RIGHT
     if (deltaX > 0) {
       const w = Math.round(parseInt(getComputedStyle(l).width) + deltaX);
-      console.log(w);
       l.style.flex = `0 ${w > 65 ? 80 : w}px`;
       r.style.flex = "1 0";
-      if( w > 65 ){ 
-        textIcon.forEach (text => {
+      if (w > 65) {
+        textIcon.forEach(text => {
           text.classList.remove("hiddenH");
         })
       }
-    }  
-  } 
+    }
+  }
 }
 
 function onmouseup(e) {
@@ -181,21 +192,21 @@ function onmousemoveY(e) {
   const t = resizerY.previousElementSibling;
   const b = resizerY.nextElementSibling;
 
-  if(clientY > screen.height){
+  if (clientY > screen.height) {
     resizerY.clientY = screen.height;
   }
-  else if(clientY < 0){
+  else if (clientY < 0) {
     resizerY.clientY = 0;
   }
-  else{
+  else {
     resizerY.clientY = clientY;
     // DOWN
     if (deltaY > 0) {
       const h = Math.round(parseInt(getComputedStyle(b).height) - deltaY);
       b.style.flex = `0 ${h < 55 ? 50 : h}px`;
       t.style.flex = "1 0";
-      if( Math.round(parseInt(getComputedStyle(t).height) + deltaY) > 290 ){ 
-        textIcon.forEach (text => {
+      if (Math.round(parseInt(getComputedStyle(t).height) + deltaY) > 290) {
+        textIcon.forEach(text => {
           text.classList.remove("hiddenV");
         })
       }
@@ -205,24 +216,231 @@ function onmousemoveY(e) {
       const h = Math.round(parseInt(getComputedStyle(t).height) + deltaY);
       t.style.flex = `0 ${h < 155 ? 145 : h}px`;
       b.style.flex = "1 0";
-      if( h < 290 ){ 
-        textIcon.forEach (text => {
+      if (h < 290) {
+        textIcon.forEach(text => {
           text.classList.add("hiddenV");
         })
       }
     }
-    
+
   }
 }
 function onmouseupY(e) {
-  e.preventDefault(); 
+  e.preventDefault();
   document.removeEventListener("mousemove", onmousemoveY);
   document.removeEventListener("mouseup", onmouseupY);
   delete e._clientY;
 }
 
+/* Editor and tabs */
+//adding new editor tabs
+const addTab = document.querySelector(".ide__tabs__add")
+const tabsLeftContainer = document.querySelector(".ide__tabs__left")
+let closeTabs = document.querySelectorAll(".close-tab")
+let tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
+let ideList = document.querySelectorAll(".editor")
 
-/* main page functionality */
+let i = 1
+let j = 1
+
+//Function to create new tabs
+function createTab(tabNumber) {
+  const newTab = document.createElement("li")
+  newTab.setAttribute("data-tab-id", tabNumber)
+  newTab.setAttribute('id', 'tab');
+
+  const tabContent = document.createElement("p")
+  tabContent.innerText = `Thing Description ${tabNumber}`
+  tabContent.classList.add("content-tab")
+  // tabContent.setAttribute("contenteditable", "false")
+  const closeBtn = document.createElement("div")
+  closeBtn.classList.add("close-tab")
+
+  const closeIcon = document.createElement("i")
+  closeIcon.classList.add("fa-solid", "fa-xmark")
+
+  closeBtn.appendChild(closeIcon)
+  newTab.appendChild(tabContent)
+  newTab.appendChild(closeBtn)
+
+  tabsLeftContainer.insertBefore(newTab, tabsLeftContainer.children[(tabsLeftContainer.children.length) - 1])
+  tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
+}
+
+function createIde(ideNumber){
+  const newIde = document.createElement("div")
+  newIde.classList.add("editor")
+  newIde.setAttribute('id', `editor${ideNumber}`);
+  newIde.setAttribute("data-ide-id", ideNumber)
+  ideContainer.appendChild(newIde)
+
+  require.config({ paths: { vs: '../monaco-editor/min/vs' } });
+  require(['vs/editor/editor.main'], function () {
+
+    var editor = monaco.editor.create(document.getElementById(`editor${ideNumber}`), {
+      value: ['{',
+        '  "id": "urn:simple",',
+        '  "@context": "https://www.w3.org/2022/wot/td/v1.1",',
+        `  "title": "My Thing ${ideNumber}",`,
+        '  "description": "Empty thing description",',
+        '  "securityDefinitions": {',
+        '    "basic_sc": {',
+        '      "scheme": "basic",',
+        '      "in": "header"',
+        '    }',
+        '  },',
+        '  "security": [',
+        '    "basic_sc"',
+        '  ],',
+        '  "properties": {',
+        '  },',
+        '  "actions": {',
+        '  },',
+        '  "events": {',
+        '  }',
+        '}'].join('\n'),
+      language: 'json',
+      automaticLayout: true
+    });
+    document.onload = setTheme()
+    document.onload = setFontSize(editor)
+    fontSizeSlider.addEventListener("input", () => {
+      fontSizeTxt.innerText = fontSizeSlider.value
+      storeFontSize(fontSizeSlider.value)
+      setFontSize(editor)
+    })
+    editorForm.addEventListener("reset", (e) => {
+      e.preventDefault()
+      fontSizeSlider.value = 14
+      storeFontSize(fontSizeSlider.value)
+      setFontSize(editor)
+    })
+  })
+
+  ideList = document.querySelectorAll(".editor")
+}
+
+//Create a new tab when clicking on the plus tab
+const ideContainer = document.querySelector(".ide__container")
+addTab.addEventListener("click", () => {
+  createTab(++i)
+  createIde(i)
+})
+
+//Getting the taget when clicking on the tabs container
+tabsLeftContainer.addEventListener("click", (e) => {
+  const selectedElement = e.target
+
+  //Add the active styling when tab clicked
+  if (selectedElement.id == "tab" || selectedElement.parentElement.id == "tab") {
+
+    tabsLeft.forEach(tab => {
+      tab.classList.remove("active")
+      tab.children[0].removeAttribute("contenteditable")
+    })
+
+    ideList.forEach(ide => {
+      ide.classList.remove("active")
+    })
+
+    if (selectedElement.id == "tab") {
+      selectedElement.classList.add("active")
+    }
+    else {
+      selectedElement.parentElement.classList.add("active")
+    }
+
+    if(selectedElement.dataset.tabId){
+      ideList.forEach(ide => {
+        if(selectedElement.dataset.tabId === ide.dataset.ideId){
+          ide.classList.add("active")
+        }
+      })
+    }
+    else{
+      ideList.forEach(ide => {
+        if(selectedElement.parentElement.dataset.tabId === ide.dataset.ideId){
+          ide.classList.add("active")
+        }
+      })
+    }
+  }
+  
+  //Closing tabs only when the click event happens on the close icon of the tab
+  if (selectedElement.className == "close-tab" && tabsLeft.length >= 1) {
+    //If there is only one more tab and its closed create a completely new one
+    //If not the last one adjust the styling accordingly and update the amount of tabs
+    if(selectedElement.parentElement.dataset.tabId){
+      ideList.forEach(ide => {
+        if(selectedElement.parentElement.dataset.tabId === ide.dataset.ideId){
+          ide.remove()
+        }
+      })
+    }
+    if (tabsLeft.length == 1) {
+      i = 0
+      selectedElement.parentElement.remove()
+      createTab(++i)
+      createIde(i)
+      tabsLeft[0].classList.add("active")
+      ideList[0].classList.add("active")
+    }
+    else {
+      if (selectedElement.parentElement.className == "active") {
+        selectedElement.parentElement.remove()
+        tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
+        ideList = document.querySelectorAll(".editor")
+        tabsLeft[0].classList.add("active")
+        ideList[0].classList.add("active")
+      }
+      else {
+        selectedElement.parentElement.remove()
+        tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
+      }
+    }
+  }
+})
+
+tabsLeftContainer.addEventListener("dblclick", (e) => {
+  const selectedElement = e.target
+
+  if (selectedElement.className == "content-tab") {
+    selectedElement.setAttribute("contenteditable", "true")
+    selectedElement.focus()
+  }
+
+})
+
+/***  Setting menu ***/
+const editorForm = document.querySelector(".settings__editor")
+const themePicker = document.querySelector("#theme-picker")
+const fontSizeTxt = document.querySelector(".editor-font-size")
+const fontSizeSlider = document.querySelector("#font-size")
+
+fontSizeTxt.innerText = fontSizeSlider.value
+
+// fontSizeSlider.addEventListener("input", () => {
+//   fontSizeTxt.innerText = fontSizeSlider.value
+//   storeFontSize(fontSizeSlider.value)
+//   setFontSize(editor)
+// })
+
+editorForm.addEventListener("reset", (e) => {
+  e.preventDefault()
+  fontSizeSlider.value = 14
+  fontSizeTxt.innerText = fontSizeSlider.value
+  themePicker.value = "light-mode"
+  storeTheme(themePicker.value)
+  storeFontSize(fontSizeSlider.value)
+  document.documentElement.className = themePicker.value
+  if (themePicker.value == "dark-mode") {
+    monaco.editor.setTheme('vs-dark')
+  }else if (themePicker.value == "light-mode") {
+    monaco.editor.setTheme('vs')
+  }else{
+    monaco.editor.setTheme('monochrome')
+  }
+})
 
 //settings menu
 closeSettings.addEventListener("click", () => {
@@ -233,119 +451,49 @@ settingsBtn.addEventListener("click", () => {
   settingsMenu.classList.toggle("closed")
 })
 
-//adding new editor views
-const addTab = document.querySelector(".ide__tabs__add")
-const tabsLeftContainer = document.querySelector(".ide__tabs__left")
-let closeTabs = document.querySelectorAll(".close-tab")
-let tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
-
-let i = 1
-let j = 1
-addTab.addEventListener("click",  () => {
-  createTab(++i)
-})
-
-tabsLeftContainer.addEventListener("click", (e) => {
-  const selectedElement = e.target
-  
-
-  if(selectedElement.id == "tab"){
-    tabsLeft.forEach(tab => {
-      tab.classList.remove("active")
-    })
-    selectedElement.classList.add("active")
-  }
-
-  if(selectedElement.className == "close-tab" && tabsLeft.length > 1){
-    if(selectedElement.parentElement.className == "active"){
-      selectedElement.parentElement.remove()
-      tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
-      tabsLeft[0].classList.add("active")
-    }
-    else{
-      selectedElement.parentElement.remove()
-    }
-  }
-
-  if(selectedElement.className == "close-tab" && tabsLeft.length < 1){
-    i = 0
-    createTab(++i)
-    tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
-    tabsLeft[0].classList.add("active")
-  }
-
-})
-
-
-function createTab (tabNumber){
-  const newTab = document.createElement("li")
-  newTab.innerText =`Thing Description ${tabNumber}`
-  newTab.setAttribute("data-tab", `${((newTab.innerText).toLowerCase()).replace(/\s/g, '')}`)
-  newTab.setAttribute('id','tab');
-  const closeBtn = document.createElement("div")
-  closeBtn.classList.add("close-tab")
-  const closeIcon = document.createElement("i")
-  closeIcon.classList.add("fa-solid", "fa-xmark")
-  closeBtn.appendChild(closeIcon)
-  newTab.appendChild(closeBtn)
-
-  tabsLeftContainer.insertBefore(newTab, tabsLeftContainer.children[(tabsLeftContainer.children.length) - 1])
-  tabsLeft = document.querySelectorAll(".ide__tabs__left li:not(:last-child)")
-}
-
-/** Themes picker functionality **/
-const themePicker = document.querySelector("#theme-picker")
+/* Themes picker and font picker functionality */
 
 //Store the selected theme
-const storeTheme = function(theme) {
+const storeTheme = function (theme) {
   localStorage.setItem("theme", theme)
 }
 
-const setTheme = function() {
+const storeFontSize = function (fontSize) {
+  localStorage.setItem("fontSize", fontSize)
+}
+
+//Function which get the value from the localStorage and sets the new theme
+const setTheme = function () {
   const activeTheme = localStorage.getItem("theme")
   themePicker.value = activeTheme
   document.documentElement.className = activeTheme
 
-  if(activeTheme == "dark-mode"){
+  if (activeTheme == "dark-mode") {
     monaco.editor.setTheme('vs-dark')
-  }
-  
-  if(activeTheme == "light-mode"){
+  }else if(activeTheme == "light-mode") {
     monaco.editor.setTheme('vs')
-  }
-
-  if(activeTheme == "monochrome-mode"){
+  }else{
     monaco.editor.setTheme('monochrome')
   }
 }
 
+const setFontSize = function (editor) {
+  const activeFontSize = localStorage.getItem("fontSize")
+  editor.updateOptions({
+    fontSize: activeFontSize
+  })
+}
+
+//Event listener to change the theme when the select is changed
 themePicker.addEventListener("change", () => {
   storeTheme(themePicker.value)
   document.documentElement.className = themePicker.value
 
-  if(themePicker.value == "dark-mode"){
+  if (themePicker.value == "dark-mode") {
     monaco.editor.setTheme('vs-dark')
-  }
-  
-  if(themePicker.value == "light-mode"){
+  }else if (themePicker.value == "light-mode") {
     monaco.editor.setTheme('vs')
-  }
-
-  if(themePicker.value == "monochrome-mode"){
+  }else{
     monaco.editor.setTheme('monochrome')
   }
-  
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
